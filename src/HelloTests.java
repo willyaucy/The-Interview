dcf831d17f752b1f2a054288d7fad0236177a317
YAUYIN <yauyinx@gmail.com>
2016-08-13 03:03:23 -0700

    Initial Commit

---
+++ src/HelloTests.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by yau on 8/13/16.
+ */
+public class HelloTests {
+  @Test
+  public void test1() {
+    assertEquals("1", "1");
+  }
+
+  @Test
+  public void test2() {
+//    assertEquals("1", "2");
+  }
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by yau on 8/13/16.
+ */
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello");
+  }
+}
