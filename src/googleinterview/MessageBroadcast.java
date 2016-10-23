80b22f17a702058c0b2c3cc99c279ba3178dc236
William Yau <yauyinx@gmail.com>
2016-10-23 16:07:47 -0700

    probably correct

--- src/googleinterview/MessageBroadcast.java
+++ src/googleinterview/MessageBroadcast.java
@@ -2,6 +2,12 @@ package googleinterview;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -9,6 +15,13 @@ import static org.junit.Assert.assertEquals;
  * Each city is represented as an integer ranging from 0 to n.
  * The cost of sending the message from i to j is costs[i][j], where costs is a square matrix.
  * Find the minimum cost of sending the message from one location to all other cities.
+ *
+ *
+ * Started Oct 23 3:10PM
+ * Finished Oct 23 3:52PM
+ *
+ * Approach:
+ * Kruskal's algorithm. Turns out they want me to study CS61B and memorize Kruskal. lololol
  */
 public class MessageBroadcast {
   /**
@@ -18,7 +31,61 @@ public class MessageBroadcast {
    * @return the cost
    */
   public static int broadcast(int origin, int[][] costs) {
-    return -1;
+    List<Edge> edges = new ArrayList<>();
+
+    for (int i = 0; i < costs.length; i++) {
+      for (int j = i + 1; j < costs.length; j++) {
+        edges.add(new Edge(i, j, costs[i][j]));
+      }
+    }
+
+    edges.sort((e1, e2) -> e1.cost - e2.cost);
+
+    boolean[][] isConnected = new boolean[costs.length][costs.length];
+    int minimumCost = 0;
+
+    for (Edge edge : edges) {
+      if (!hasPath(edge.from, edge.to, isConnected)) {
+        minimumCost += edge.cost;
+        isConnected[edge.from][edge.to] = true;
+        isConnected[edge.to][edge.from] = true;
+      }
+    }
+
+    return minimumCost;
+  }
+
+  public static boolean hasPath(int i1, int i2, boolean[][] graph) {
+    LinkedList<Integer> nodesToExpand = new LinkedList<>();
+    Set<Integer> visited = new HashSet<>();
+    nodesToExpand.offer(i1);
+
+    while (!nodesToExpand.isEmpty()) {
+      int node = nodesToExpand.poll();
+      visited.add(node);
+
+      if (node == i2) {
+        return true;
+      } else {
+        for (int i = 0; i < graph.length; i++) {
+          if (graph[node][i] && !visited.contains(i)) {
+            nodesToExpand.offer(i);
+          }
+        }
+      }
+    }
+
+    return false;
+  }
+
+  private static class Edge {
+    final int from, to, cost;
+
+    public Edge(int from, int to, int cost) {
+      this.from = from;
+      this.to = to;
+      this.cost = cost;
+    }
   }
 
   public static class MessageBroadcastTest {
