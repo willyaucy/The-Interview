1a4d775320e6005c5a91f2150e9c03dd63132125
William Yau <yauyinx@gmail.com>
2017-01-03 19:24:57 +0800

    Memoized solution

--- src/main/java/Abbreviation.java
+++ src/main/java/Abbreviation.java
@@ -1,9 +1,12 @@
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Scanner;
 import java.util.stream.Stream;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -52,36 +55,81 @@ public class Abbreviation {
    * @return
    */
   public static boolean canAbbr(String a, String b) {
-    if (a.length() == b.length()) {
-      return a.toUpperCase().equals(b);
-    } else if (a.length() < b.length()) {
-      return false;
+    return new Abbreviator(a, b).canAbbr();
+  }
+
+  public static class Abbreviator {
+    /**
+     * Maps from (a, b) to result
+     */
+    final Map<Pair<String, String>, Boolean> memo;
+
+    private final String a, b;
+
+    public Abbreviator(String a, String b) {
+      memo = new HashMap<>();
+      this.a = a;
+      this.b = b;
     }
 
-    if (Character.toUpperCase(a.charAt(0)) == b.charAt(0)) {
-      if (Character.isUpperCase(a.charAt(0))) {
-        return canAbbr(a.substring(1), b.substring(1));
-      } else {
-        return canAbbr(a.substring(1), b.substring(1)) || canAbbr(a.substring(1), b);
-      }
+    public boolean canAbbr() {
+      return canAbbr(a, b);
     }
 
-    if (Character.toUpperCase(a.charAt(a.length() - 1)) == b.charAt(b.length() - 1)) {
-      if (Character.isUpperCase(a.charAt(a.length() - 1))) {
-        return canAbbr(a.substring(0, a.length() - 1), b.substring(0, b.length() - 1));
-      } else {
-        return canAbbr(a.substring(0, a.length() - 1), b.substring(0, b.length() - 1))
-            || canAbbr(a.substring(0, a.length() - 1), b);
-      }
-    }
+    private boolean canAbbr(String a, String b) {
+      return memo.computeIfAbsent(new Pair<>(a, b), abPair -> {
+        if (a.length() == b.length()) {
+          return a.toUpperCase().equals(b);
+        } else if (b.length() == 0) {
+          return true;
+        } else if (a.length() < b.length()) {
+          return false;
+        }
 
-    return canAbbr(a.substring(1, a.length() - 1), b);
+        if (Character.toUpperCase(a.charAt(0)) == b.charAt(0)) {
+          if (Character.isUpperCase(a.charAt(0))) {
+            return canAbbr(a.substring(1), b.substring(1));
+          } else {
+            return canAbbr(a.substring(1), b.substring(1)) || canAbbr(a.substring(1), b);
+          }
+        } else if (Character.isUpperCase(a.charAt(0))) {
+          return false;
+        }
+
+        if (Character.toUpperCase(a.charAt(a.length() - 1)) == b.charAt(b.length() - 1)) {
+          if (Character.isUpperCase(a.charAt(a.length() - 1))) {
+            return canAbbr(a.substring(0, a.length() - 1), b.substring(0, b.length() - 1));
+          } else {
+            return canAbbr(a.substring(0, a.length() - 1), b.substring(0, b.length() - 1))
+                || canAbbr(a.substring(0, a.length() - 1), b);
+          }
+        } else if (Character.isUpperCase(a.charAt(a.length() - 1))) {
+          return false;
+        }
+
+        return canAbbr(a.substring(1, a.length() - 1), b);
+      });
+    }
   }
 
   public static class AbbreviationTest {
     @Test
     public void test0() {
       assertTrue(canAbbr("daBcd", "ABC"));
+    }
+
+    @Test
+    public void test1() {
+      assertTrue(canAbbr("daBbcd", "ABC"));
+    }
+
+    @Test
+    public void test2() {
+      assertTrue(canAbbr("dabbbbbBbcd", "ABC"));
+    }
+    @Test
+    public void test3() {
+      assertFalse(canAbbr("daBBcd", "ABC"));
     }
   }
 }
