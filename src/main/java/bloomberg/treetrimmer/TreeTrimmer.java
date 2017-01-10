1b49e242700483a6f7267c74c084e1dedd5975fa
William Yau <yauyinx@gmail.com>
2017-01-10 17:58:40 +0800

    Tree Trimmer without tests

---
+++ src/main/java/bloomberg/treetrimmer/TreeTrimmer.java
@@ -0,0 +1,16 @@
+package bloomberg.treetrimmer;
+
+/**
+ * Created by yauyin on 1/10/17.
+ */
+public class TreeTrimmer {
+  /**
+   * Trim a tree upto a given height and sum all the nodes below to add to the value of the leaf node.
+   * @param tree the input tree
+   * @param height the height of the returned tree
+   * @return the trimmed tree
+   */
+  public static TreeNode trim(TreeNode tree, int height) {
+    return null;
+  }
+}
